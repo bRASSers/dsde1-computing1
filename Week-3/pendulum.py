30ec6d5024e2aeb240d4dd4f52e10cff06ddff76
Mattias Erik Rass <mattias.rass19@imperial.ac.uk>
2019-10-24 14:30:10 +0100

    Some problems fixed with pendulum.py

--- Week-3/pendulum.py
+++ Week-3/pendulum.py
@@ -1,17 +1,23 @@
+# I import math in order to use pi
 import math
 
-
+# Defined the variables
 def period(L, g=9.8):
+
+    # If variables are not numbers, the user gets a TypeError with an explanation.
+    if isinstance(L, (float, int)) != True:
+        print(TypeError + 'Value entered fot L is not a nuber.')
+    if isinstance(g, (float, int)) != True:
+        print(TypeError + 'Value entered fot g is not a nuber.')  
+
+    #If the variabe for gravity is 0, the user gets a ValueError with an explanation.
+    if g == 0:
+        print(ValueError + 'Value entered for gravity can not be 0 (The pendulum would not work)')    
+      
+    # This is the equation
     T = 2*math.pi*((L/g)**0.5)
+
+    # Value for T is returned
     return T
 
-isinstance('L', (float, int))
-while False:
-    print(TypeError)
-
-isinstance('g', (float, int))
-while False:
-    print(TypeError)
-
-if 'g' == 0:
-    print(ValueError)
+print(period('lol', ))
