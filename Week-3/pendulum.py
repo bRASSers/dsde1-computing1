578fa5463349553cfca6bfe943905d131abef247
Mattias Erik Rass <mattias.rass19@imperial.ac.uk>
2019-10-28 12:18:31 +0000

    pendulum works now

--- Week-3/pendulum.py
+++ Week-3/pendulum.py
@@ -11,12 +11,12 @@ def period(L, g=9.8):
 
         #If the variabe for gravity is 0, the user gets a ValueError with an explanation.
         if g == 0:
-            print('ValueError:' + ' Value entered for gravity can not be 0 (The pendulum would not work)')
+            raise ValueError
 
         #If the variabe for L is not a number, the user gets a TypeError with an explanation.
         elif isinstance(L, (float, int)) != True:
-            print('TypeError:' + ' Value entered fot L is not a nuber.')
+            raise TypeError
 
         #If the variabe for g is not a number, the user gets a TypeError with an explanation.
         elif isinstance(g, (float, int)) != True:
-            print('TypeError:' + ' Value entered fot g is not a nuber.')
+            raise TypeError
